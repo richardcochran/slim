741006ced2fcfe550eb8baf1720d35446f03925f
Richard Cochran <richardcochran@gmail.com>
2012-06-12 20:49:28 +0200

    Move the list of debian packages into a shell script.
    The list of needed debian packages in the LaTeX file is awkward to
    maintain and use. Instead let us keep a list that is both practical
    and easy to update.
    
    Signed-off-by: Richard Cochran <richardcochran@gmail.com>

---
+++ scripts/debs.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+#
+# Convenience script to install the Debian packages needed by SLIM.
+#
+
+aptitude install \
+autoconf \
+automake \
+autotools-dev \
+bash \
+binutils \
+bison \
+bsdmainutils \
+bzip2 \
+coreutils \
+cpp \
+diff \
+diffstat \
+e2fslibs \
+e2fsprogs \
+file \
+findutils \
+flex \
+g++ \
+gawk \
+gcc \
+genext2fs \
+genisoimage \
+git-core \
+git-gui \
+gitk \
+grep \
+groff \
+groff-base \
+gzip \
+initramfs-tools \
+intltool \
+libc6 \
+libc6-dev \
+libglib2.0-dev \
+liblzo2-2 \
+liblzo2-dev \
+libncurses5 \
+libncurses5-dev \
+libtool \
+linux-libc-dev \
+lzma \
+m4 \
+make \
+mktemp \
+mount \
+mtd-utils \
+p7zip-full \
+patch \
+sed \
+tar \
+tftp \
+tftpd \
+uboot-mkimage \
+unzip \
+util-linux \
+wget \
+zip \
+zlib1g \
+zlib1g-dev
